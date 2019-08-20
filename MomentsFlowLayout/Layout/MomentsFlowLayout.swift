25d27cc615c1d5772a3cfd590130c687b944fe99
Nathan Hsu <nathan.hsu@icloud.com>
2019-08-19 19:38:58 -0700

    Successfully displays collection view items on screen.

--- MomentsFlowLayout/Layout/MomentsFlowLayout.swift
+++ MomentsFlowLayout/Layout/MomentsFlowLayout.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class MomentsFlowLayout: UICollectionViewFlowLayout {
+class MomentsFlowLayout: UICollectionViewLayout {
     
     var previousCollectionViewSize: CGSize = CGSize.zero
+    
+    /// UICollectionViewFlowLayout Properties
+    var itemSize = CGSize.zero
+    var minimumLineSpacing: CGFloat = 0
     
     var lineSpacing: CGFloat = 25
     
@@ -26,49 +30,76 @@ class MomentsFlowLayout: UICollectionViewFlowLayout {
         super.init()
         self.itemSize = itemSize
         minimumLineSpacing = lineSpacing
-        scrollDirection = .horizontal
     }
     
-    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        
-        /// Prepare initial values
-        guard let collectionView = self.collectionView else { fatalError("collectionView nil") }
-        
-        let proposedContentOffsetCenterX = proposedContentOffset.x + collectionView.bounds.width / 2
-        let proposedRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.width, height: collectionView.bounds.height)
-        
-        guard let layoutAttributes = self.layoutAttributesForElements(in: proposedRect) else { fatalError("layoutAttributes nil") }
-
-        /// Check all layoutAttributes and find the one closest to the center
-        var candidateAttributes: UICollectionViewLayoutAttributes?
-        for attributes in layoutAttributes {
-            if attributes.representedElementCategory != .cell {
-                continue
-            }
-            if candidateAttributes == nil {
-                candidateAttributes = attributes
-                continue
-            }
-            /// Check each attributes object and replaced the candidateAttributes variable if the one currently being checked has a center.x closer to the proposedContentOffsetCenterX
-            if abs(attributes.center.x - proposedContentOffsetCenterX) < abs(candidateAttributes!.center.x - proposedContentOffsetCenterX) {
-                candidateAttributes = attributes
+    override var collectionViewContentSize: CGSize {
+        guard let collectionView = self.collectionView else {
+            return CGSize.zero
+        }
+        let itemCount = collectionView.numberOfItems(inSection: 0)
+        let contentSize = CGSize(width: CGFloat(itemCount) * (itemSize.width + lineSpacing), height: collectionView.bounds.height)
+        return contentSize
+    }
+    
+//    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+//
+//        /// Prepare initial values
+//        guard let collectionView = self.collectionView else { fatalError("collectionView nil") }
+//
+//        let proposedContentOffsetCenterX = proposedContentOffset.x + collectionView.bounds.width / 2
+//        let proposedRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.width, height: collectionView.bounds.height)
+//
+//        guard let layoutAttributes = self.layoutAttributesForElements(in: proposedRect) else { fatalError("layoutAttributes nil") }
+//
+//        /// Check all layoutAttributes and find the one closest to the center
+//        var candidateAttributes: UICollectionViewLayoutAttributes?
+//        for attributes in layoutAttributes {
+//            if attributes.representedElementCategory != .cell {
+//                continue
+//            }
+//            if candidateAttributes == nil {
+//                candidateAttributes = attributes
+//                continue
+//            }
+//            /// Check each attributes object and replaced the candidateAttributes variable if the one currently being checked has a center.x closer to the proposedContentOffsetCenterX
+//            if abs(attributes.center.x - proposedContentOffsetCenterX) < abs(candidateAttributes!.center.x - proposedContentOffsetCenterX) {
+//                candidateAttributes = attributes
+//            }
+//        }
+//        guard let aCandidateAttributes = candidateAttributes else {
+//            return proposedContentOffset
+//        }
+//
+//        /// Calculate newOffsetX
+//        var newOffsetX = aCandidateAttributes.center.x - collectionView.bounds.size.width / 2
+//
+//        /// Adjust newOffsetX to allow small flicks to advance at least a full page in the flick direction
+//        let offset = newOffsetX - collectionView.contentOffset.x
+//        if (velocity.x < 0 && offset > 0) || (velocity.x > 0 && offset < 0) {
+//            let pageWidth = itemSize.width + minimumLineSpacing
+//            newOffsetX += velocity.x > 0 ? pageWidth : -pageWidth
+//        }
+//
+//        return CGPoint(x: newOffsetX, y: proposedContentOffset.y)
+//    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        attributes.frame = CGRect(x: CGFloat(indexPath.row)*(itemSize.width + lineSpacing), y: 70, width: itemSize.width, height: itemSize.height)
+        return attributes
+    }
+    
+    var layoutAttributes: [UICollectionViewLayoutAttributes] = []
+    
+    override func prepare() {
+        let itemCount = collectionView!.numberOfItems(inSection: 0)
+        for i in 0..<itemCount {
+            let indexPath = IndexPath(row: i, section: 0)
+            if let attribute = layoutAttributesForItem(at: indexPath) {
+                layoutAttributes.append(attribute)
             }
         }
-        guard let aCandidateAttributes = candidateAttributes else {
-            return proposedContentOffset
-        }
         
-        /// Calculate newOffsetX
-        var newOffsetX = aCandidateAttributes.center.x - collectionView.bounds.size.width / 2
-        
-        /// Adjust newOffsetX to allow small flicks to advance at least a full page in the flick direction
-        let offset = newOffsetX - collectionView.contentOffset.x
-        if (velocity.x < 0 && offset > 0) || (velocity.x > 0 && offset < 0) {
-            let pageWidth = itemSize.width + minimumLineSpacing
-            newOffsetX += velocity.x > 0 ? pageWidth : -pageWidth
-        }
-        
-        return CGPoint(x: newOffsetX, y: proposedContentOffset.y)
     }
     
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
@@ -76,28 +107,32 @@ class MomentsFlowLayout: UICollectionViewFlowLayout {
         
         let largerRect = CGRect(x: 0, y: 0, width: 20000, height: ourCollectionView.frame.height)
         
-        guard
-            let collectionView = self.collectionView,
-            let superAttributes = super.layoutAttributesForElements(in: largerRect) else { return super.layoutAttributesForElements(in: largerRect) }
+        guard let collectionView = self.collectionView else { fatalError("collectionView nil") }
+//        guard let superAttributes = super.layoutAttributesForElements(in: largerRect) else { return super.layoutAttributesForElements(in: largerRect) }
         
         let contentOffset = collectionView.contentOffset
         let size = collectionView.bounds.size
         let visibleRect = CGRect(x: contentOffset.x, y: contentOffset.y, width: size.width, height: size.height)
         let visibleCenterX = visibleRect.midX
         
-        guard case let newAttributesArray as [UICollectionViewLayoutAttributes] = NSArray(array: superAttributes, copyItems: true) else { return nil }
+//        guard case let newAttributesArray as [UICollectionViewLayoutAttributes] = NSArray(array: superAttributes, copyItems: true) else { return nil }
+        let newAttributesArray = layoutAttributes
         
-        newAttributesArray.forEach {
-            let distanceFromCenter = -(visibleCenterX - $0.center.x)
-            let scale = distanceFromCenter * (self.scaleFactor - 1) / self.scaleOffset + 1
-            let amountToShift: CGFloat = distanceFromCenter > 0 ? -distanceFromCenter * 0.85 : distanceFromCenter * 0.01
-            let transform1 = CATransform3DTranslate(CATransform3DIdentity, amountToShift-16, 0, 0)
-            let transform2 = CATransform3DScale(transform1, scale, scale, 0)
-            $0.transform3D = transform2
-            $0.zIndex = -$0.indexPath.row
+        let filteredArray = newAttributesArray.filter { (attributes) -> Bool in
+            attributes.frame.intersects(rect)
         }
         
-        return newAttributesArray
+//        newAttributesArray.forEach {
+//            let distanceFromCenter = -(visibleCenterX - $0.center.x)
+//            let scale = distanceFromCenter * (self.scaleFactor - 1) / self.scaleOffset + 1
+//            let amountToShift: CGFloat = distanceFromCenter > 0 ? -distanceFromCenter * 0.85 : distanceFromCenter * 0.01
+//            let transform1 = CATransform3DTranslate(CATransform3DIdentity, amountToShift-16, 0, 0)
+//            let transform2 = CATransform3DScale(transform1, scale, scale, 0)
+//            $0.transform3D = transform2
+//            $0.zIndex = -$0.indexPath.row
+//        }
+        
+        return filteredArray
     }
     
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
